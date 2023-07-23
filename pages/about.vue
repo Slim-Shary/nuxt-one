62d3f5ddab0d1443b9b9412bac98e6e1ce5711e5
Slim-Shary <shahryarmon@gmail.com>
2023-07-22 23:35:29 -0700

    add pages/routes

---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">On to the nuxt one</NuxtLink>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">Products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <h2>About</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
