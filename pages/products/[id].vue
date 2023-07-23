81a0aa33281b2bf309f423cd840cd47d35d97005
Slim-Shary <shahryarmon@gmail.com>
2023-07-23 11:25:24 -0700

    add data fetching and reusable components

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <p>Product details for {{ id }}</p>
+    <ProductDetails :product="product" />
   </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params;
+const uri = `https://fakestoreapi.com/products/${id}`;
+
+const { data: product } = await useFetch(uri, { key: id });
 definePageMeta({
   layout: 'products',
 });
