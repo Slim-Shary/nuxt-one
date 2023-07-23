4ab93cf69e830739708f4396bafc1972c815e022
Slim-Shary <shahryarmon@gmail.com>
2023-07-23 12:42:13 -0700

    set dynamic server routes

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h2>About</h2>
+    <div>{{ data }}</div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const { data } = await useFetch('/api/currency/GBP');
+</script>
 
 <style scoped>
 h2 {
